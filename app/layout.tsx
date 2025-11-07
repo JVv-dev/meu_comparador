1b869ea8a24a9ba25ccfd1739d3906e39b0ccfcb
JVv-dev <joaovitorvenero913@gmail.com>
2025-11-07 19:01:36 -0300

    Titulo da pagina alterado

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'GPU price',
+  title: 'price comparison',
   description: 'Encontre as melhores ofertas de placas de vídeo e mais!',
   other: { 
     "google": "notranslate"
