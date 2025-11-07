9a93b1ed6bc3fcdb0e1f99f67a8a15939f82d651
JVv-dev <joaovitorvenero913@gmail.com>
2025-11-07 19:21:45 -0300

    Imagem  alterada

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'price comparison',
+  title: 'Price Comparison',
   description: 'Encontre as melhores ofertas de placas de vídeo e mais!',
   other: { 
     "google": "notranslate"
