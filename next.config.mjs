e3317a5e134b254db4fa21deb40e8194f0c8ba9e
JVv-dev <joaovitorvenero913@gmail.com>
2025-11-17 16:44:13 -0300

    imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
+// Conteúdo para: meu_comparador_frontend/next.config.mjs
+// (v11.1 - Autorizando Imagens Externas)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // --- INÍCIO DA MUDANÇA (AUTORIZAR IMAGENS) ---
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.terabyteshop.com.br',
+        port: '',
+        pathname: '/archive/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hotsite.pichau.com.br',
+        port: '',
+        pathname: '/descricao/**',
+      },
+      // Adicione aqui se a Kabum também tiver imagens na descrição
+    ],
   },
-}
+  // --- FIM DA MUDANÇA ---
+};
 
-export default nextConfig
+export default nextConfig;
