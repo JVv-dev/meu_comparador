f638070fcacb091ac6dfe7c23ae574befd6814fe
JVv-dev <joaovitorvenero913@gmail.com>
2025-11-17 17:30:01 -0300

    consert 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,23 @@
+// Conteúdo para: meu_comparador_frontend/next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // --- INÍCIO DA MUDANÇA (AUTORIZAR IMAGENS) ---
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'img.terabyteshop.com.br',
-        port: '',
-        pathname: '/archive/**',
+        hostname: 'img.terabyteshop.com.br', // Para imagens da Terabyte
       },
       {
         protocol: 'https',
-        hostname: 'hotsite.pichau.com.br',
-        port: '',
-        pathname: '/descricao/**',
+        hostname: 'hotsite.pichau.com.br', // Para imagens da Pichau
       },
       {
         protocol: 'https',
-        hostname: 'hotsite.kabum.com.br',
-        port: '',
-        pathname: '/descricao/**',
+        hostname: 'hotsite.kabum.com.br', // Para imagens da Kabum (Boa adição!)
       },
     ],
   },
-  // --- FIM DA MUDANÇA ---
 };
 
 export default nextConfig;
