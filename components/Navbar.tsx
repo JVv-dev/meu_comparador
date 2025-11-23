35c9bf416e437e8f29452dbf37ffa83bac6d1e99
JVv-dev <joaovitorvenero913@gmail.com>
2025-11-23 20:57:32 -0300

    Feat(V3.0): Adiciona pagina de Cupons e Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center space-x-2 font-bold text-xl">
             <ShoppingBag className="w-6 h-6 text-primary" />
-            <span className="hidden md:inline-block">Produtos</span>
+            <span className="hidden md:inline-block">Produdos</span>
           </Link>
           
           <div className="flex gap-2">
